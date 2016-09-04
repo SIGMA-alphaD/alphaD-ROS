bae664cb9ed891fcd9425fe3dd8228eef9cc4807
dydwo92 <dydwo92@snu.ac.kr>
2016-09-05 06:58:56 +0900

    update imu filter

--- control-pkg/imu_filter/src/imu_filter/ahrs_node.py
+++ control-pkg/imu_filter/src/imu_filter/ahrs_node.py
@@ -4,26 +4,35 @@ import madgwick_filter
 from sensor_msgs.msg import Imu
 
 _pub = rospy.Publisher('info/imu/data', Imu, queue_size = 10)
+_sampleRate = 400 # 400Hz
 
-_Filter = madgwick_filter.MadgwickAHRS()
+_Filter = madgwick_filter.MadgwickAHRS(1.0/_sampleRate)
+
+_prevT = -1
 
 def callback(data):
     global _Filter
     global _pub
+    global _prevT
+    global _sampleRate
 
+    if _prevT is not -1:
+        if (data.header.nsec - _prevT) >= 1000000.0/_sampleRate :
+            acc = [data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z]
+            gyro = [data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z]
 
-    acc = [data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z]
-    gyro = [data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z]
+            _Filter.update_imu(gyro, acc)
+            msg = Imu()
+            msg.header.frame_id = "imu"
+            msg.orientation.w = _Filter.quaternion._q[0]
+            msg.orientation.x = _Filter.quaternion._q[1]
+            msg.orientation.y = _Filter.quaternion._q[2]
+            msg.orientation.z = _Filter.quaternion._q[3]
 
-    _Filter.update_imu(gyro, acc)
-    msg = Imu()
-    msg.header.frame_id = "imu"
-    msg.orientation.w = _Filter.quaternion._q[0]
-    msg.orientation.x = _Filter.quaternion._q[1]
-    msg.orientation.y = _Filter.quaternion._q[2]
-    msg.orientation.z = _Filter.quaternion._q[3]
-
-    _pub.publish(msg)
+            _pub.publish(msg)
+            _prevT = _prevT + 1000000.0/_sampleRate
+    else :
+        _prevT = data.header.nsec;
 
 def listener():
     rospy.init_node('imu_filter_run')
