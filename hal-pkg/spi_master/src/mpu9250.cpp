f5c431b25ce8eec2a73e14437173c51e4687fc61
dydwo92 <dydwo92@snu.ac.kr>
2016-09-05 07:44:17 +0900

    Update mpu9250.cpp

--- hal-pkg/spi_master/src/mpu9250.cpp
+++ hal-pkg/spi_master/src/mpu9250.cpp
@@ -7,22 +7,24 @@ mpu9250::handler::handler(){
 	fd = 0;
 }
 
-#define MPU_InitRegNum 14
+#define MPU_InitRegNum 17
 int mpu9250::handler::Setup(void){
     int i;
 
     unsigned char MPU_Init_Data[MPU_InitRegNum][2] ={
-        //{0x80, MPUREG_PWR_MGMT_1}, // Reset
-        {0x01, MPUREG_PWR_MGMT_1}, // Auto select clock source
-        {0x00, MPUREG_PWR_MGMT_2}, // Acc & Gyro enable
-        {0x10, MPUREG_GYRO_CONFIG}, // +-1000dps
-        {0x00, MPUREG_ACCEL_CONFIG}, // +-2G
-        {0x30, MPUREG_INT_PIN_CFG},
+        {BIT_H_RESET, MPUREG_PWR_MGMT_1},     // Reset Device
+        {0x01, MPUREG_PWR_MGMT_1},     // Clock Source
+        {0x00, MPUREG_PWR_MGMT_2},     // Enable Acc & Gyro
+        {my_low_pass_filter, MPUREG_CONFIG},         // Use DLPF set Gyroscope bandwidth 184Hz, temperature bandwidth 188Hz
+        {BITS_FS_250DPS, MPUREG_GYRO_CONFIG},    // +-250dps
+        {BITS_FS_2G, MPUREG_ACCEL_CONFIG},   // +-2G
+        {my_low_pass_filter_acc, MPUREG_ACCEL_CONFIG_2}, // Set Acc Data Rates, Enable Acc LPF , Bandwidth 184Hz
+        {0x30, MPUREG_INT_PIN_CFG},    //
         //{0x40, MPUREG_I2C_MST_CTRL},   // I2C Speed 348 kHz
         //{0x20, MPUREG_USER_CTRL},      // Enable AUX
         {0x20, MPUREG_USER_CTRL},       // I2C Master mode
         {0x0D, MPUREG_I2C_MST_CTRL}, //  I2C configuration multi-master  IIC 400KHz
-
+        
         {AK8963_I2C_ADDR, MPUREG_I2C_SLV0_ADDR},  //Set the I2C slave addres of AK8963 and set for write.
         //{0x09, MPUREG_I2C_SLV4_CTRL},
         //{0x81, MPUREG_I2C_MST_DELAY_CTRL}, //Enable I2C delay
@@ -32,7 +34,11 @@ int mpu9250::handler::Setup(void){
         {0x81, MPUREG_I2C_SLV0_CTRL},  //Enable I2C and set 1 byte
 
         {AK8963_CNTL1, MPUREG_I2C_SLV0_REG}, //I2C slave 0 register address from where to begin data transfer
-        {0x12, MPUREG_I2C_SLV0_DO}, // Register value to continuous measurement in 16bit
+#ifdef AK8963FASTMODE
+        {0x16, MPUREG_I2C_SLV0_DO}, // Register value to 100Hz continuous measurement in 16bit
+#else
+        {0x12, MPUREG_I2C_SLV0_DO}, // Register value to 8Hz continuous measurement in 16bit
+#endif
         {0x81, MPUREG_I2C_SLV0_CTRL}  //Enable I2C and set 1 byte
     };
 
@@ -45,10 +51,91 @@ int mpu9250::handler::Setup(void){
 
     for(i=0;i<MPU_InitRegNum;i++){
         mpu9250Write(MPU_Init_Data[i][0], MPU_Init_Data[i][1]);
-		delayMicroseconds(10000);
+		delayMicroseconds(1000);
     }
 	
+        set_acc_scale(BITS_FS_2G);
+    	set_gyro_scale(BITS_FS_1000DPS);
 	return fd;
+}
+
+/*                                ACCELEROMETER SCALE
+ * usage: call this function at startup, after initialization, to set the right range for the
+ * accelerometers. Suitable ranges are:
+ * BITS_FS_2G
+ * BITS_FS_4G
+ * BITS_FS_8G
+ * BITS_FS_16G
+ * returns the range set (2,4,8 or 16)
+ */
+
+unsigned int MPU9250::handler::set_acc_scale(int scale){
+    unsigned int temp_scale;
+    WriteReg(MPUREG_ACCEL_CONFIG, scale);
+    
+    switch (scale){
+        case BITS_FS_2G:
+            acc_divider=16384;
+        break;
+        case BITS_FS_4G:
+            acc_divider=8192;
+        break;
+        case BITS_FS_8G:
+            acc_divider=4096;
+        break;
+        case BITS_FS_16G:
+            acc_divider=2048;
+        break;   
+    }
+    temp_scale = WriteReg(MPUREG_ACCEL_CONFIG|READ_FLAG, 0x00);
+    
+    switch (temp_scale){
+        case BITS_FS_2G:
+            temp_scale=2;
+        break;
+        case BITS_FS_4G:
+            temp_scale=4;
+        break;
+        case BITS_FS_8G:
+            temp_scale=8;
+        break;
+        case BITS_FS_16G:
+            temp_scale=16;
+        break;   
+    }
+    return temp_scale;
+}
+
+/*                                 GYROSCOPE SCALE
+ * usage: call this function at startup, after initialization, to set the right range for the
+ * gyroscopes. Suitable ranges are:
+ * BITS_FS_250DPS
+ * BITS_FS_500DPS
+ * BITS_FS_1000DPS
+ * BITS_FS_2000DPS
+ * returns the range set (250,500,1000 or 2000)
+ */
+
+unsigned int MPU9250::handler::set_gyro_scale(int scale){
+    unsigned int temp_scale;
+    WriteReg(MPUREG_GYRO_CONFIG, scale);
+
+    switch (scale){
+        case BITS_FS_250DPS:   gyro_divider = 131;  break;
+        case BITS_FS_500DPS:   gyro_divider = 65.5; break;
+        case BITS_FS_1000DPS:  gyro_divider = 32.8; break;
+        case BITS_FS_2000DPS:  gyro_divider = 16.4; break;   
+    }
+
+    temp_scale = WriteReg(MPUREG_GYRO_CONFIG|READ_FLAG, 0x00);
+
+    switch (temp_scale){
+        case BITS_FS_250DPS:   temp_scale = 250;    break;
+        case BITS_FS_500DPS:   temp_scale = 500;    break;
+        case BITS_FS_1000DPS:  temp_scale = 1000;   break;
+        case BITS_FS_2000DPS:  temp_scale = 2000;   break;   
+    }
+    return temp_scale;
 }
 
 unsigned char mpu9250::handler::mpu9250Read(unsigned char regNum){
